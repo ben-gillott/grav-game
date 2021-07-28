8b2ffe77bb21263205f2a25dbd6e92bef3c33582
Benjamin Gillott <bg357@cornell.edu>
2021-07-28 15:27:44 -0400

    Gravity Controller v1
    Flips gravity and moves around

---
+++ Gravity/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    //Not so sure what it do
+    public static T GetOrAddComponent<T>(this GameObject gameObject) where T: Component
+    {
+        if(gameObject.TryGetComponent(out T requestedComponent)){
+            return requestedComponent;
+        }
+        return gameObject.AddComponent<T>();
+    }
+
+    //The Anakin method
+    public static void DestroyChildren(this Transform transform){
+        foreach(Transform child in transform){
+            Object.Destroy(child.gameObject);
+        }
+    }
+}
---
+++ Gravity/Assets/Scripts/GravityController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public float gravity = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            Physics2D.gravity = new Vector2(0f, gravity);
+        }
+        else if(Input.GetKey(KeyCode.A)){
+            Physics2D.gravity = new Vector2(-gravity, 0f);
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            Physics2D.gravity = new Vector2(0f, -gravity);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            Physics2D.gravity = new Vector2(gravity, 0f);
+        }
+    }
+}
