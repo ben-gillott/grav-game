c61dfc0887d555c570c4ab7432178b7861016af0
Ben Gillott <benjaminrgillott@gmail.com>
2022-04-04 21:44:50 -0700

    Acceleration Changes

--- godot_grav/scripts/PlayerMovementController.gd
+++ godot_grav/scripts/PlayerMovementController.gd
@@ -1,12 +1,11 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 20
 
-const GRAVITY = 10 #in px
-var velocity = Vector2()
-
+const GRAVITY = 200 #in px
+var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
-	
+
 func _input(event):
 	#Movement
 	if event.is_action_pressed("ui_left"):
@@ -23,18 +22,16 @@ func _input(event):
 		pass #TODO: Interact
 
 func movePlayer(inputDirection):
-	#Grounded -> Can jump
-	
+	#TODO: Check Grounded -> Can jump
 	direction = inputDirection
 	
 func _physics_process(delta):
-	
-	#TODO: rethink this
 	#only allows velocity in one cardinal direction at a time 
 	if velocity.normalized() != direction:
-		velocity = Vector2.ZERO
+		#velocity = Vector2.ZERO #stop moving
+		velocity = direction * speed
 	
 	print(str(velocity))
 		
-	velocity += direction * GRAVITY
+	velocity += direction * GRAVITY * delta
 	velocity = move_and_slide(velocity)
